6b0cfa1ea9ead54c357b4120d6d3c07657560097
Chirs Greene <chigreenwork@gmail.com>
2023-08-27 14:14:14 -0400

    adds html and script file

---
+++ script.js
@@ -0,0 +1,11 @@
+// dictionary api
+
+
+
+
+// joke api
+
+var jokeContainer = document.getElementById('jokeContainer')
+var btn = document.getElementById('btn');
+var url = 'https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,racist,sexist,explicit';
+
