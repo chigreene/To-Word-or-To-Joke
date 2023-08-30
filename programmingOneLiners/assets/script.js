9bf9de889bbfe11356f372756a6cc0ee0e0c1981
Chirs Greene <chigreenwork@gmail.com>
2023-08-30 06:38:24 -0400

    need to hear it again button only renders the previous joke

--- programmingOneLiners/assets/script.js
+++ programmingOneLiners/assets/script.js
@@ -1,7 +1,9 @@
 var jokeContainer = document.getElementById('jokeContainer')
 var btn = document.getElementById('btn');
 var lastJokeBtn = document.querySelector('#lastJokeBtn')
+var saveBtn = document.querySelector('#saveBtn')
 var lastJokeDiv = document.querySelector('#lastJoke')
+var array = JSON.parse(localStorage.getItem('jokeHistory')) || [];
 
 
 function makeJoke(){
@@ -14,31 +16,39 @@ function makeJoke(){
             return response.json();
         })
         .then(function (data){
-            console.log(data)
             jokeContainer.textContent = ''
-            console.log(data)
             var joke = document.createElement('p');
             joke.textContent = data.joke
             jokeContainer.append(joke)
             jokeHistory = data.joke
-            localStorage.setItem('jokeHistory', jokeHistory)
+            saveToArray(jokeHistory)
         })
 }
 
-var lastJokePara = document.createElement('p')
+function saveToArray(joke) {
+    if(!array.includes(joke)){
+        array.push(joke);
+        localStorage.setItem('jokeHistory', JSON.stringify(array));
+    }
+}
+var lastJokePara = document.createElement('p');
+function renderHistory() {
+    var localHistoryArray = JSON.parse(localStorage.getItem('jokeHistory'));
+    
+    lastJokePara.textContent = '';
+    lastJokePara.textContent = localHistoryArray[array.length-2];
+    lastJokeDiv.append(lastJokePara);
+}
 
-btn.addEventListener('click',function(){
-        makeJoke()
+
+
+btn.addEventListener('click',function(event){
+    event.preventDefault()
+    makeJoke()
 })
 
 lastJokeBtn.addEventListener('click', function(event){
     event.preventDefault()
-    function lastJoke(){
-        var lastJoke = localStorage.getItem('jokeHistory')
-       
-        lastJokePara.textContent = ''
-        lastJokePara.textContent = lastJoke
-        lastJokeDiv.append(lastJokePara)
-    }
-    lastJoke()
+    
+    renderHistory()
 })
