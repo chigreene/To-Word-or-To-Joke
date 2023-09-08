ab6f0f9fa00150676a5371724e7e2bcb96d26077
Venoude Valmyr <venoudevalmyr@gmail.com>
2023-09-07 20:18:54 -0400

    included dictionary text box

--- dictionaryscript.js
+++ dictionaryscript.js
@@ -1,31 +1,51 @@
 var searchDictionary = $('#Dictionary');
 var searchBtn = $('#search');
-var input = searchDictionary.value;
+var defCont = $('.defContainer');
 
 
-searchBtn.addEventListener('click', function(event) {
+searchBtn.on('click', function(event) {
     event.preventDefault();
+    var input = searchDictionary.val();
     console.log(input);
+    getApi();
 });
 
 
-// function searchBarClck(event) {
-//     event.preventDefault();
-//     console.log(searchDictionary.val());
+function getApi() {
+    //fetch request
+    var input = searchDictionary.val();
+    var requestUrl =`https://dictionaryapi.com/api/v3/references/collegiate/json/${input}?key=303749c6-a0fc-48e0-9b98-5bedab74e635`;
 
-//     $('#search').val('');
+    fetch(requestUrl)
+    .then(function (response) {
+        console.log(response);
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+        var shortDef = data[0].shortdef;
+        console.log(shortDef);
+    
+        $('.defCont').empty();
+        var showWord = $('<h1></h1>').text(input);
+        $('.wordOfTheDay').append(showWord);
 
-// }
-
-// searchDictionary.on('click', searchBarClck);
-
+        for(let i=0; i < shortDef.length; i++) {
+            var element = $('<p></p>').text(shortDef[i]);
+            $('.wordOfTheDay').append(element);
+        }
+    })
+}
 
 // function getApi() {
-//     //fetch request
-//     var requestUrl = 'https://www.dictionaryapi.com/api/v3/references/collegiate/json/voluminous?key=your-api-key'
-
+//     var freebeeURL = '/cgi-bin/today';
 //     fetch(requestUrl)
 //     .then(function (response) {
+//         console.log(response);
 //         return response.json();
 //     })
+//     .then(function (data) {
+//         console.log(data);
+// })
+
 // }
